693f56509147ccd0120f56e1aa7a1e780195da35
César Augustus Alborghetti <caugustusalborghetti@gmail.com>
2024-06-24 17:51:49 -0300

    Criado o programa para calculo de salarios.

---
+++ CalcularSalario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaSalario
+{
+    internal class CalcularSalario
+    {
+        public string Nome;
+        public double Bruto;
+        public double Imposto;
+        public double Porcentagem;
+
+        public double SalarioLiquido()
+        {
+            return Bruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            Bruto += Bruto * porcentagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+            + " R$ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ProgramaSalario;
+using System.Globalization;
+
+namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalcularSalario calc = new CalcularSalario();
+        Console.WriteLine("Digite o nome do contribuinte: ");
+        calc.Nome = Console.ReadLine();
+        Console.WriteLine("Digite o salário bruto: ");
+        calc.Bruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Digite o imposto a ser cobrado:");
+        calc.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Funcionario: " + calc);
+
+        Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+        double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        calc.AumentarSalario(porcent);
+
+        Console.WriteLine("Salario atualizado: " + calc);
+
+    }
+}
